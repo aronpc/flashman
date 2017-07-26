a46c29e0cb71e2643e0d93e5dcc4a4e4c5d15123
Guilherme Senges <guisenges@gmail.com>
2017-07-26 00:56:45 -0300

    Fixed old route named users and renamed to deviceinfo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var deviceinfo = require('./routes/deviceinfo');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/deviceinfo', deviceinfo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
