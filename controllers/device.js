c22689a232b75ad866f458e599abb3b2d8c7c514
Guilherme Senges <guisenges@gmail.com>
2017-07-27 20:21:23 -0300

    Fixed query callback since find returns empty if it has no matches

--- controllers/device.js
+++ controllers/device.js
@@ -3,42 +3,46 @@ var deviceModel = require('../models/device');
 var deviceController = {};
 
 var createRegistry = function(req) {
-    newDeviceModel = new deviceModel({'id': req.query.id,
-                                      'model': req.query.model,
-                                      'version': req.query.version,
-                                      'ip': req.ip,
-                                      'last_contact': Date.now(),
-                                      'do_update': false,
-                                     });
-    newDeviceModel.save(function (err) {
-        if (err) {
-            return false;
-        } else {
-            return true;
-        }
-    });
+  newDeviceModel = new deviceModel({'id': req.query.id,
+                                    'model': req.query.model,
+                                    'version': req.query.version,
+                                    'ip': req.ip,
+                                    'last_contact': Date.now(),
+                                    'do_update': false,
+                                  });
+  newDeviceModel.save(function (err) {
+    if (err) {
+      return false;
+    } else {
+      return true;
+    }
+  });
 };
 
+// Create new device entry or update an existing one
 deviceController.updateDevicesInfo = function(req, res) {
-    deviceModel.findOne({'id': req.query.id}, function(err, matchedDevice) {
-        if(err) {
-            console.log('No device found. Creating registry...');
-            createdStatus = createRegistry(req);
-            if(createdStatus) {
-                res.status(200).json({'do_update': false});
-            } else {
-                res.status(500);
-            }
+  deviceModel.findOne({'id': req.query.id}, function(err, matchedDevice) {
+    if(err) {
+      console.log('Error finding device' + err);
+      return res.status(500);
+    } else {
+      if(matchedDevice == null){
+        createdStatus = createRegistry(req);
+        if(createdStatus) {
+          return res.status(200).json({'do_update': false});
         } else {
-            console.log(matchedDevice);
-            deviceModel.update({'model': req.query.model,
-                                'version': req.query.version,
-                                'ip': req.ip,
-                                'last_contact': Date.now()
-                               });
-            res.status(200).json({'do_update': matchedDevice.do_update});
+          return res.status(500);
         }
-    });
+      } else {
+        deviceModel.update({'model': req.query.model,
+                            'version': req.query.version,
+                            'ip': req.ip,
+                            'last_contact': Date.now()
+                           });
+        return res.status(200).json({'do_update': matchedDevice.do_update});
+      }
+    }
+  });
 };
 
 module.exports = deviceController;
