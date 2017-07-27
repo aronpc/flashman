f95a802cb84670ef2538a06a694e4555b9d2db2a
Guilherme Senges <guisenges@gmail.com>
2017-07-27 20:23:38 -0300

    Reindenting to 2 spaces

--- models/device.js
+++ models/device.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var deviceSchema = new Schema({
-    id : String,
-    model : String,
-    version : String,
-    ip : String,
-    last_contact : Date,
-    do_update : Boolean
+  id : String,
+  model : String,
+  version : String,
+  ip : String,
+  last_contact : Date,
+  do_update : Boolean
 });
 
 var Device = mongoose.model('Device', deviceSchema );
