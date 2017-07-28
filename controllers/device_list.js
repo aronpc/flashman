5f04613fe214f3b8ccd95c781c1a80e8c7bb05bb
Guilherme Senges <guisenges@gmail.com>
2017-07-28 01:56:54 -0300

    Uncommented query on controller

--- controllers/device_list.js
+++ controllers/device_list.js
@@ -5,17 +5,15 @@ var deviceListController = {};
 // List all devices on a main page
 deviceListController.index = function(req, res) {
   var indexContent = {apptitle: 'FlashMan'};
-  console.log("Aqui");
-  // deviceModel.find(function(err, devices) {
-  //   if(err) {
-  //     indexContent.message = err.message;
-  //     console.log("Aqisasdas");
-  //     return res.render('error', indexContent);
-  //   }
-  //   console.log("ADADDA");
-  //   indexContent.devices = devices;
-  //   return res.render('index', indexContent);
-  // });
+
+  deviceModel.find(function(err, devices) {
+    if(err) {
+      indexContent.message = err.message;
+      return res.render('error', indexContent);
+    }
+    indexContent.devices = devices;
+    return res.render('index', indexContent);
+  });
   return res.render('index', indexContent);
 };
 
