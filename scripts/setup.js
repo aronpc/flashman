108aacb66b816d510753d4d94857c95ff21bb741
Guilherme Senges <guisenges@gmail.com>
2017-12-20 15:14:52 -0200

    Added proper message when asking for superuser modification

--- scripts/setup.js
+++ scripts/setup.js
@@ -5,7 +5,6 @@ var mongoose = require('mongoose');
 var user = require('../models/user');
 
 const imageReleasesDir = require('../config/configs').imageReleasesDir;
-const mqttBrokerURL = require('../config/configs').mqttBrokerURL;
 
 mongoose.connect('mongodb://localhost:27017/flashman', {useMongoClient: true});
 
@@ -36,7 +35,6 @@ var createSuperUser = function() {
             }
             console.log('User successfully created!');
             mongoose.connection.close();
-            checkMQTTBrokerURL();
           });
         } else {
           matchedUser.password = result.password;
@@ -46,23 +44,10 @@ var createSuperUser = function() {
             }
             console.log('User successfully edited!');
             mongoose.connection.close();
-            checkMQTTBrokerURL();
           });
         }
       });
   });
-};
-
-var createMQTTBrokerURL = function() {
-  prompt.get([{
-      name: 'mqttbrokerurl',
-      message: 'New MQTT broker URL',
-      validator: /^mqtt\:\/\/[a-zA-Z0-9\-\/\:\.]+$/,
-      warning: 'Always include mqtt://',
-      required: true
-    }], function (err, result) {
-      require('../config/configs').mqttBrokerURL = result.mqttbrokerurl;
-    });
 };
 
 var confirmSuperUserCreation = function(hasSuperUser) {
@@ -80,43 +65,12 @@ var confirmSuperUserCreation = function(hasSuperUser) {
           createSuperUser();
         } else {
           mongoose.connection.close();
-          checkMQTTBrokerURL();
         }
     });
   } else {
     createSuperUser();
   }
 };
-
-var confirmMQTTBrokerURLCreation = function(hasBrokerURL) {
-  var proceed = true;
-
-  if(hasBrokerURL) {
-    prompt.get([{
-        name: 'editwanted',
-        message: 'Do you want to change the MQTT broker URL? (true/false)',
-        type: 'boolean',
-        warning: 'Type true or false',
-        required: true
-      }], function (err, result) {
-        if(result.editwanted) {
-          createMQTTBrokerURL();
-        }
-    });
-  } else {
-    createMQTTBrokerURL();
-  }
-};
-
-var checkMQTTBrokerURL = function() {
-  console.log('Checking if MQTT broker URL exists...');
-  var hasBrokerURL = false;
-  if (mqttBrokerURL.startsWith("mqtt://")) {
-    console.log('MQTT broker URL found!');
-    hasBrokerURL = true;
-  } 
-  confirmMQTTBrokerURLCreation(hasBrokerURL);
-}
 
 console.log('Checking directories...')
 if (!fs.existsSync(imageReleasesDir)) {
