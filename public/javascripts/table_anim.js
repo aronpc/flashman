c59755b8a479ea84958a66be3ed9d9e5be382d10
Guilherme Senges <guilherme@anlix.io>
2018-03-19 02:21:34 -0300

    Fixed chevron animation Fixed dropdown selected release

--- public/javascripts/table_anim.js
+++ public/javascripts/table_anim.js
@@ -1,24 +1,28 @@
 
 $(document).ready(function() {
-    $('.glyphicon-menu-right').click(function() {
-        $(this).toggleClass("glyphicon-menu-down");
-    });
+  $('.fa-chevron-right').parents('td').click(function() {
+    if ($(this).children().hasClass('fa-chevron-right')) {
+      $(this).find('.fa-chevron-right')
+        .removeClass('fa-chevron-right')
+        .addClass('fa-chevron-down');
+    } else if ($(this).children().hasClass('fa-chevron-down')) {
+      $(this).find('.fa-chevron-down')
+        .removeClass('fa-chevron-down')
+        .addClass('fa-chevron-right');
+    }
+  });
 
-    $('.glyphicon-menu-down').click(function() {
-        $(this).toggleClass("glyphicon-menu-right");
+  $('.btn-trash').click(function(event) {
+    let row = $(event.target).parents('tr');
+    let id = row.data('deviceid');
+    $.ajax({
+      url: '/devicelist/delete/' + id,
+      type: 'post',
+      success: function(res) {
+        setTimeout(function() {
+          window.location.reload();
+        }, 100);
+      },
     });
-
-    $('.btn-trash').click(function(event) {
-      var row = $(event.target).parents('tr');
-      var id = row.data('deviceid');
-      $.ajax({
-          url: '/devicelist/delete/' + id,
-          type: 'post',
-          success: function(res) {
-            setTimeout(function(){
-              window.location.reload();
-            },100);
-          }
-      });
-    });
+  });
 });
--- public/javascripts/update_device.js
+++ public/javascripts/update_device.js
@@ -1,49 +1,49 @@
 
-var updateDevice = function(event) {
+let updateDevice = function(event) {
   let row = $(event.target).parents('tr');
   let id = row.prop('id');
   let release = row.find('span.selected').text();
   $(event.target).prop('disabled', true);
   $.ajax({
-      url: 'devicelist/update/' + id + '/' + release,
-      type: 'post',
-      success: function(res) {
-        if (res.success) {
-          $(event.target).prop('disabled', false);
-        }
-      },
+    url: 'devicelist/update/' + id + '/' + release,
+    type: 'post',
+    success: function(res) {
+      if (res.success) {
+        $(event.target).prop('disabled', false);
+      }
+    },
   });
 };
 
-var updateAllDevices = function(event) {
+let updateAllDevices = function(event) {
   let row = $(event.target).parents('tr');
-  let is_checked = $(event.target).is(':checked');
+  let isChecked = $(event.target).is(':checked');
   let rows = row.siblings();
-  let ids_obj = {};
+  let idsObj = {};
   rows.each(function(idx) {
     if ($(this).prop('id') !== undefined && $(this).prop('id').length > 0) {
       let id = $(this).prop('id');
       let rel = $(this).find('span.selected').text();
-      ids_obj[id] = rel;
+      idsObj[id] = rel;
     }
   });
   $(event.target).prop('disabled', true);
   $.post('devicelist/updateall',
-         {content: JSON.stringify({ids: ids_obj, do_update: is_checked})})
+         {content: JSON.stringify({ids: idsObj, do_update: isChecked})})
     .done(function(res) {
       if (res.success) {
-        $('.checkbox').prop('checked', is_checked);
+        $('.checkbox').prop('checked', isChecked);
         $(event.target).prop('disabled', false);
       }
     });
 };
 
-var refreshRelease = function(event) {
-  let selected = $(event.target).parents('.dropdown').find('span.selected');
-  selected.text($(this).text() + ' ');
+let refreshRelease = function(event) {
+  let selected = $(event.target).parents('.btn-group').find('span.selected');
+  selected.text($(this).text());
   // Check if firmware update is enabled. If not, enable it.
-  let is_checked = $(event.target).parents('tr').find('.checkbox').is(':checked');
-  if (is_checked) {
+  let isChecked = $(event.target).parents('tr').find('.checkbox').is(':checked');
+  if (isChecked) {
     updateDevice(event);
   } else {
     // Trigger slider animation
