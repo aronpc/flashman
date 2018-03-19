68fadb9e22ab4672041e00572bec6d089d380dd8
Guilherme Senges <guilherme@anlix.io>
2018-03-19 01:05:40 -0300

    Using Google standard

--- public/javascripts/update_device.js
+++ public/javascripts/update_device.js
@@ -1,29 +1,29 @@
 
 var updateDevice = function(event) {
-  var row = $(event.target).parents('tr');
-  var id = row.prop('id');
-  var release = row.find('span.selected').text();
+  let row = $(event.target).parents('tr');
+  let id = row.prop('id');
+  let release = row.find('span.selected').text();
   $(event.target).prop('disabled', true);
   $.ajax({
       url: 'devicelist/update/' + id + '/' + release,
       type: 'post',
       success: function(res) {
-        if(res.success) {
+        if (res.success) {
           $(event.target).prop('disabled', false);
         }
-      }
+      },
   });
 };
 
 var updateAllDevices = function(event) {
-  var row = $(event.target).parents('tr');
-  var is_checked = $(event.target).is(':checked');
-  var rows = row.siblings();
-  var ids_obj = {};
+  let row = $(event.target).parents('tr');
+  let is_checked = $(event.target).is(':checked');
+  let rows = row.siblings();
+  let ids_obj = {};
   rows.each(function(idx) {
-    if($(this).prop('id') !== undefined && $(this).prop('id').length > 0) {
-      var id = $(this).prop('id');
-      var rel = $(this).find('span.selected').text();
+    if ($(this).prop('id') !== undefined && $(this).prop('id').length > 0) {
+      let id = $(this).prop('id');
+      let rel = $(this).find('span.selected').text();
       ids_obj[id] = rel;
     }
   });
@@ -31,7 +31,7 @@ var updateAllDevices = function(event) {
   $.post('devicelist/updateall',
          {content: JSON.stringify({ids: ids_obj, do_update: is_checked})})
     .done(function(res) {
-      if(res.success) {
+      if (res.success) {
         $('.checkbox').prop('checked', is_checked);
         $(event.target).prop('disabled', false);
       }
@@ -39,10 +39,10 @@ var updateAllDevices = function(event) {
 };
 
 var refreshRelease = function(event) {
-  var selected = $(event.target).parents('.dropdown').find('span.selected');
+  let selected = $(event.target).parents('.dropdown').find('span.selected');
   selected.text($(this).text() + ' ');
   // Check if firmware update is enabled. If not, enable it.
-  var is_checked = $(event.target).parents('tr').find('.checkbox').is(':checked');
+  let is_checked = $(event.target).parents('tr').find('.checkbox').is(':checked');
   if (is_checked) {
     updateDevice(event);
   } else {
