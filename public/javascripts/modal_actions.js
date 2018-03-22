0f724d647f8a31fef7f3d9e80ab8b4e42a2c608c
Guilherme Senges <guilherme@anlix.io>
2018-03-22 20:34:28 -0300

    Replaced default alert notification for modal notification

---
+++ public/javascripts/modal_actions.js
@@ -0,0 +1,9 @@
+
+const displayAlertMsg = function(res) {
+  $('#frame-modal-alert .modal-dialog').removeClass(
+    'modal-success modal-danger'
+  );
+  $('#frame-modal-alert .modal-dialog').addClass('modal-' + res.type);
+  $('#frame-modal-alert .alert-message').html(res.message);
+  $('#frame-modal-alert').modal('show');
+};
