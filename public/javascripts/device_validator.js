6e1c4bf13c80cc39a5ec87a81fd34653924870db
Thurler <gt@anlix.io>
2018-05-03 00:31:59 -0300

    Small fix for icons on chrome and wifi password validation

--- public/javascripts/device_validator.js
+++ public/javascripts/device_validator.js
@@ -1,10 +1,10 @@
 (function() {
   let deviceValidator = (function () {
-    let validateRegex = function(value, length, regex) {
+    let validateRegex = function(value, minlength, length, regex) {
       let valid = true;
       let err = [];
 
-      if (value.length < 1) {
+      if (value.length < minlength) {
         valid = false;
         err.push(0);
       }
@@ -43,18 +43,18 @@
         "Este campo não pode ter mais de 64 caracteres",
         "Somente são aceitos: caracteres alfanuméricos, espaços, - e _"
       ];
-      let ret = validateRegex(user, 64, /^[a-zA-Z0-9\-\_\#\s]+$/);
+      let ret = validateRegex(user, 1, 64, /^[a-zA-Z0-9\-\_\#\s]+$/);
       ret.err = ret.err.map(ind => messages[ind]);
       return ret;
     };
 
     Validator.prototype.validatePassword = function(pass) {
       const messages = [
-        "Este campo é obrigatório",
+        "Este campo deve ter no mínimo 8 caracteres",
         "Este campo não pode ter mais de 64 caracteres",
         "Letras com acento, cedilha, e alguns caracteres especiais não são aceitos"
       ];
-      let ret = validateRegex(pass, 64, /^[a-zA-Z0-9\-\_\#\!\@\$\%\&\*\=\+\?]+$/);
+      let ret = validateRegex(pass, 8, 64, /^[a-zA-Z0-9\-\_\#\!\@\$\%\&\*\=\+\?]+$/);
       ret.err = ret.err.map(ind => messages[ind]);
       return ret;
     };
@@ -65,18 +65,18 @@
         "Este campo não pode ter mais de 32 caracteres",
         "Somente são aceitos: caracteres alfanuméricos, espaços, - e _"
       ];
-      let ret = validateRegex(ssid, 32, /^[a-zA-Z0-9\-\_\#\s]+$/);
+      let ret = validateRegex(ssid, 1, 32, /^[a-zA-Z0-9\-\_\#\s]+$/);
       ret.err = ret.err.map(ind => messages[ind]);
       return ret;
     };
 
     Validator.prototype.validateWifiPassword = function(pass) {
       const messages = [
-        "Este campo é obrigatório",
+        "Este campo deve ter no mínimo 8 caracteres",
         "Este campo não pode ter mais de 64 caracteres",
         "Letras com acento, cedilha, e alguns caracteres especiais não são aceitos"
       ];
-      let ret = validateRegex(pass, 64, /^[a-zA-Z0-9\-\_\#\!\@\$\%\&\*\=\+\?]+$/);
+      let ret = validateRegex(pass, 8, 64, /^[a-zA-Z0-9\-\_\#\!\@\$\%\&\*\=\+\?]+$/);
       ret.err = ret.err.map(ind => messages[ind]);
       return ret;
     };
