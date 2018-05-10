184937fac54e3cbaa211bb09c0f04614b1f9eee8
Guilherme Senges <guilherme@anlix.io>
2018-05-10 20:34:24 -0300

    - Changed color of buttons used on popup notifications - Improved file JS formatting using ESLinter

--- public/javascripts/upgrade_flashman.js
+++ public/javascripts/upgrade_flashman.js
@@ -1,9 +1,9 @@
-let forceUpdateFlashman = function () {
+let forceUpdateFlashman = function() {
   swal({
     title: 'Atualizando Flashman...',
     onOpen: () => {
       swal.showLoading();
-    }
+    },
   });
 
   $.ajax({
@@ -18,42 +18,45 @@ let forceUpdateFlashman = function () {
         swal({
           type: 'success',
           title: 'Atualização feita com sucesso!',
-          text: 'Você precisará fazer login novamente'
-        }).then(function () {
-          window.location.href = "/logout";
+          text: 'Você precisará fazer login novamente',
+          confirmButtonColor: '#f57c00',
+        }).then(function() {
+          window.location.href = '/logout';
         });
-      }
-      else {
+      } else {
         swal({
           type: 'error',
-          title: 'Erro ao atualizar'
+          title: 'Erro ao atualizar',
+          confirmButtonColor: '#f57c00',
         });
       }
-    }
+    },
   });
 };
 
-let alertUpdateFlashman = function () {
+let alertUpdateFlashman = function() {
   swal({
     type: 'warning',
     title: 'Atualização disponível!',
     text: 'Deseja instalar a nova versão agora?',
     confirmButtonText: 'Atualizar',
+    confirmButtonColor: '#f57c00',
     cancelButtonText: 'Agora não',
-    showCancelButton: true
-  }).then(function (result) {
+    cancelButtonColor: '#f2ab63',
+    showCancelButton: true,
+  }).then(function(result) {
     if (result.value) {
       forceUpdateFlashman();
     }
   });
 };
 
-let checkUpdateFlashman = function () {
+let checkUpdateFlashman = function() {
   swal({
     title: 'Buscando atualizações...',
     onOpen: () => {
       swal.showLoading();
-    }
+    },
   });
 
   $.ajax({
@@ -66,14 +69,14 @@ let checkUpdateFlashman = function () {
       swal.close();
       if (resp.hasUpdate) {
         alertUpdateFlashman();
-      }
-      else {
+      } else {
         swal({
           type: 'error',
-          title: 'Nenhuma atualização encontrada'
+          title: 'Nenhuma atualização encontrada',
+          confirmButtonColor: '#f57c00',
         });
       }
-    }
+    },
   });
 };
 
@@ -85,13 +88,14 @@ let configFlashman = function() {
       swal({
         title: 'Configurações',
         input: 'checkbox',
-        inputValue: (resp.auto === true) ? true : false,  // needed since can be null
+        inputValue: (resp.auto === true) ? true : false, // needed since can be null
         inputPlaceholder: 'Deixar que o Flashman se atualize automaticamente',
-        confirmButtonText: 'Salvar Alterações'
-      }).then(function (result) {
+        confirmButtonText: 'Salvar Alterações',
+        confirmButtonColor: '#f57c00',
+      }).then(function(result) {
         if ('value' in result) {
           $.ajax({
-            type: "POST",
+            type: 'POST',
             url: '/upgrade/config',
             dataType: 'json',
             data: JSON.stringify({auto: result.value}),
@@ -99,17 +103,18 @@ let configFlashman = function() {
             success: function(resp) {
               swal({
                 type: 'success',
-                title: 'Alterações feitas com sucesso'
+                title: 'Alterações feitas com sucesso',
+                confirmButtonColor: '#f57c00',
               });
-            }
+            },
           });
         }
       });
-    }
+    },
   });
 };
 
 $(document).ready(function() {
-  $(".update").click(checkUpdateFlashman);
-  $(".config").click(configFlashman);
+  $('.update').click(checkUpdateFlashman);
+  $('.config').click(configFlashman);
 });
