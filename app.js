15a0d9060bf841caf835d397ecbf59940b514cd3
Gaspare <gaspare@anlix.io>
2018-06-07 12:58:27 -0300

    Change log to show ip address

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger(':req[x-forwarded-for] - :method :url HTTP/:http-version :status :res[content-length] - :response-time ms'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
