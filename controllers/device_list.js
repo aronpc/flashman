cd879105ab1dc92d1bcc29b20eb81c5eae7d266a
Guilherme Senges <guilherme@anlix.io>
2018-07-03 00:30:27 -0300

    Fixed API responses out of our standard

--- controllers/device_list.js
+++ controllers/device_list.js
@@ -309,10 +309,10 @@ deviceListController.searchDeviceReg = function(req, res) {
 deviceListController.delDeviceReg = function(req, res) {
   DeviceModel.remove({_id: req.params.id.toUpperCase()}, function(err) {
     if (err) {
-      return res.status(500).json({'success': false,
-                                   'message': 'device cannot be removed'});
+      return res.status(500).json({success: false,
+                                   message: 'Entrada não pode ser removida'});
     }
-    return res.status(200).json({'success': true});
+    return res.status(200).json({success: true});
   });
 };
 
@@ -324,18 +324,20 @@ deviceListController.sendMqttMsg = function(req, res) {
   msgtype = req.params.msg.toLowerCase();
   if (msgtype == 'boot') {
     mqtt.anlix_message_router_reboot(req.params.id.toUpperCase());
-    return res.status(200).json({'message': 'Success'});
+    return res.status(200).json({success: true});
   } else if (msgtype == 'rstapp') {
     mqtt.anlix_message_router_resetapp(req.params.id.toUpperCase());
-    return res.status(200).json({'message': 'Success'}); ;
+    return res.status(200).json({success: true}); ;
   } else if (msgtype == 'rstmqtt') {
     DeviceModel.findById(req.params.id.toUpperCase(),
     function(err, matchedDevice) {
       if (err) {
-        return res.status(500).json({'message': 'internal server error'});
+        return res.status(500).json({success: false,
+                                     message: 'Erro interno do servidor'});
       }
       if (matchedDevice == null) {
-        return res.status(404).json({'message': 'device not found'});
+        return res.status(404).json({success: false,
+                                     message: 'Roteador não encontrado'});
       }
 
       // if we have a secret, remove it to allow a new one
@@ -345,15 +347,16 @@ deviceListController.sendMqttMsg = function(req, res) {
       }
 
       mqtt.anlix_message_router_resetmqtt(req.params.id.toUpperCase());
-      return res.status(200).json({'message': 'Success'});
+      return res.status(200).json({success: true});
     });
   } else if (msgtype == 'log') {
     mqtt.anlix_message_router_log(req.params.id.toUpperCase());
-    return res.status(200).json({'message': 'Success'});
+    return res.status(200).json({success: true});
   } else {
     // Message not implemented
     console.log('REST API MQTT Message not recognized ('+ msgtype +')');
-    return res.status(404).json({'message': 'Message Unrecognized'});
+    return res.status(404).json({success: false,
+                                 message: 'Esse comando não existe'});
   }
 };
 
@@ -361,10 +364,12 @@ deviceListController.getFirstBootLog = function(req, res) {
   DeviceModel.findById(req.params.id.toUpperCase(),
   function(err, matchedDevice) {
     if (err) {
-      return res.status(500).json({'message': 'internal server error'});
+      return res.status(500).json({success: false,
+                                   message: 'Erro interno do servidor'});
     }
     if (matchedDevice == null) {
-      return res.status(404).json({'message': 'device not found'});
+      return res.status(404).json({success: false,
+                                   message: 'Roteador não encontrado'});
     }
 
     if (matchedDevice.firstboot_log) {
@@ -373,7 +378,8 @@ deviceListController.getFirstBootLog = function(req, res) {
       res.end(matchedDevice.firstboot_log, 'binary');
       return res.status(200);
     } else {
-      return res.status(200).json({'message': 'No log for this device'});
+      return res.status(200).json({success: true,
+                                   message: 'Não existe log deste roteador'});
     }
   });
 };
@@ -382,10 +388,12 @@ deviceListController.getLastBootLog = function(req, res) {
   DeviceModel.findById(req.params.id.toUpperCase(),
   function(err, matchedDevice) {
     if (err) {
-      return res.status(500).json({'message': 'internal server error'});
+      return res.status(500).json({success: false,
+                                   message: 'Erro interno do servidor'});
     }
     if (matchedDevice == null) {
-      return res.status(404).json({'message': 'device not found'});
+      return res.status(404).json({success: false,
+                                   message: 'Roteador não encontrado'});
     }
 
     if (matchedDevice.lastboot_log) {
@@ -394,7 +402,8 @@ deviceListController.getLastBootLog = function(req, res) {
       res.end(matchedDevice.lastboot_log, 'binary');
       return res.status(200);
     } else {
-      return res.status(200).json({'message': 'No log for this device'});
+      return res.status(200).json({success: true,
+                                   message: 'Não existe log deste roteador'});
     }
   });
 };
@@ -403,12 +412,12 @@ deviceListController.getDeviceReg = function(req, res) {
   DeviceModel.findById(req.params.id.toUpperCase(),
   function(err, matchedDevice) {
     if (err) {
-      return res.status(500).json({'success': false,
-                                   'message': 'internal server error'});
+      return res.status(500).json({success: false,
+                                   message: 'Erro interno do servidor'});
     }
     if (matchedDevice == null) {
-      return res.status(404).json({'success': false,
-                                   'message': 'device not found'});
+      return res.status(404).json({success: false,
+                                   message: 'Roteador não encontrado'});
     }
 
     // hide secret from api
@@ -434,14 +443,14 @@ deviceListController.setDeviceReg = function(req, res) {
     if (err) {
       return res.status(500).json({
         success: false,
-        message: 'internal server error',
+        message: 'Erro interno do servidor',
         errors: [],
       });
     }
     if (matchedDevice == null) {
       return res.status(404).json({
         success: false,
-        message: 'device not found',
+        message: 'Roteador não encontrado',
         errors: [],
       });
     }
@@ -549,7 +558,7 @@ deviceListController.setDeviceReg = function(req, res) {
     } else {
       return res.status(500).json({
         success: false,
-        message: 'error parsing json',
+        message: 'Erro ao tratar JSON',
         errors: [],
       });
     }
@@ -657,7 +666,7 @@ deviceListController.createDeviceReg = function(req, res) {
   } else {
     return res.status(500).json({
       success: false,
-      message: 'Erro no json recebido',
+      message: 'Erro ao tratar JSON',
       errors: [],
     });
   }
