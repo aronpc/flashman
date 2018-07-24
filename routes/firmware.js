88b4dc93070dc3e53a101a7e8f55a67a05a96ac5
Guilherme Senges <guilherme@anlix.io>
2018-07-24 12:09:59 -0300

    Applied ES Linter JS using Google standard

--- routes/firmware.js
+++ routes/firmware.js
@@ -1,16 +1,17 @@
 
-var express = require('express');
-var router = express.Router();
-var firmwareController = require('../controllers/firmware');
-var authController = require('../controllers/auth');
+const express = require('express');
+const firmwareController = require('../controllers/firmware');
+const authController = require('../controllers/auth');
+
+let router = express.Router();
 
 router.route('/').get(authController.ensureLogin(),
                       firmwareController.firmwares);
 
 router.route('/del').post(authController.ensureLogin(),
-                          firmwareController.delFirmware)
+                          firmwareController.delFirmware);
 
 router.route('/upload').post(authController.ensureLogin(),
-                             firmwareController.uploadFirmware)
+                             firmwareController.uploadFirmware);
 
 module.exports = router;
